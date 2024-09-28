c08412a918132903db7444800605c9d86722445e
CerberusV1 <jurkfranzalexander@gmail.com>
2024-09-28 23:17:07 +0200

    made the update script work

--- .config/qtile/assets/update.sh
+++ .config/qtile/assets/update.sh
@@ -1,18 +1,51 @@
 #!/bin/bash
 
-#alacritty -e ./.config/qtile/assets/update.sh
-cat << "EOF"
-              ____            _                   _   _           _       _       
-             / ___| _   _ ___| |_ ___ _ __ ___   | | | |_ __   __| | __ _| |_ ___ 
-             \___ \| | | / __| __/ _ \ '_ ` _ \  | | | | '_ \ / _` |/ _` | __/ _ \
-              ___) | |_| \__ \ ||  __/ | | | | | | |_| | |_) | (_| | (_| | ||  __/
-             |____/ \__, |___/\__\___|_| |_| |_|  \___/| .__/ \__,_|\__,_|\__\___|
-                    |___/                              |_|                     
+# System Update
+figlet -m-2 "Updating System..."
+sudo pacman -Syu 
 
-                    
-                       
-EOF
+# Confirmation for system update
+read -p "Do you want to proceed with the system update? (y/n): " -n 1 -r
+echo    # New line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sudo pacman -Syu --noconfirm
+else
+    echo "System update canceled."
+    exit 1
+fi
 
-read -p "Press [Enter] to exit..."
+# Update AUR Packages (optional)
+figlet "Updating AUR Packages..."
+if command -v yay &> /dev/null; then
+    echo "Available AUR updates:"
+    yay -Syu
 
-pause
+    # Confirmation for AUR update
+    read -p "Do you want to proceed with the AUR update? (y/n): " -n 1 -r
+    echo    # New line
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        yay -Syu --noconfirm
+    else
+        echo "AUR update canceled."
+    fi
+fi
+
+# Update Flatpak Packages
+figlet "Updating Flatpaks..."
+if command -v flatpak &> /dev/null; then
+    flatpak update --assumeyes 
+
+    # Confirmation for Flatpak update
+    read -p "Do you want to proceed with the Flatpak update? (y/n): " -n 1 -r
+    echo    # New line
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        flatpak update -y
+    else
+        echo "Flatpak update canceled."
+    fi
+else
+    echo "Flatpak is not installed."
+fi
+
+figlet "Update done."
+read -p "  Press [Enter] to exit..."
